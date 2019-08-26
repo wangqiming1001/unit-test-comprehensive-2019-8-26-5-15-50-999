8daf5be714dcbc7a534ad92d79a44e149d6acb86
wangqiming1001 <512821390@qq.com>
2019-08-26 20:51:03 +0800

    main function

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -1,5 +1,8 @@
 package tw;
 
+
+import java.util.List;
+
 import tw.commands.GuessInputCommand;
 
 /**
@@ -10,9 +13,14 @@ public class Main {
     private static final int RUN_TIMES = 6;
 
     public static void main(String[] args) throws Exception {
+    	
+    	List<Integer> list = GuessInputCommand.random();
+    	System.out.println(list);
         for (int i = 0; i < RUN_TIMES; i++) {
-            String input = new GuessInputCommand().input();
-            System.out.println(input);
+        	GuessInputCommand.answer();
+        	String answer = GuessInputCommand.sameNumber();
+        	System.out.println(answer);
         }
+        System.out.println("游戏结束");
     }
 }
--- src/main/java/tw/commands/GuessInputCommand.java
+++ src/main/java/tw/commands/GuessInputCommand.java
@@ -3,6 +3,11 @@ package tw.commands;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.annotation.Retention;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by jxzhong on 2017/5/19.
@@ -10,13 +15,56 @@ import java.io.InputStreamReader;
 public class GuessInputCommand {
 
     private BufferedReader bufferedReader;
+    private static List<Integer> list = new ArrayList<Integer>();
+    private static List<String> answer = new ArrayList<String>();
+    
 
     public GuessInputCommand() {
         bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     }
 
     public String input() throws IOException {
-        System.out.println("------Please input your answer as x x x x , x <10 ------");
+        System.out.println("------Please input your answer as 1234 , x <10 ------");
         return bufferedReader.readLine();
     }
+    
+    //生成4个随机数放入list
+    public static List<Integer> random() {
+    	//生成答案 如：1234
+        for(int i=0;i<4;i++){
+            int random = new Random().nextInt(10);
+            list.add(random);
+        }
+        return list;
+	}
+    
+    //输入的字符串变为数组
+    public static List<String> answer() throws IOException {
+    	 String input = new GuessInputCommand().input();
+    	 List<String> answer = Arrays.asList(input.split(""));
+    	 return answer;
+	}
+    
+    //02 统计相同数字(包括位置)
+    public static String sameNumber() {
+    	int countA = 0;
+    	int countB = 0;
+    	for (int i = 0; i < answer.size(); i++) {
+    		for (int j = 0; j < list.size(); j++) {
+    			System.out.println(Integer.parseInt(answer.get(i)));
+    			if (Integer.parseInt(answer.get(i)) == list.get(j)) {
+    				countB++;
+    				if (i == j) {
+    					countA++;
+					}
+    			}
+    		}
+		
+    	}
+    	String buffer = countA+"A"+countB+"B";
+    	return buffer;
+	}
+    
+    
+    
 }
